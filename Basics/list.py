632a77a625aaced081ddc98b0fd1005492d8e0e6
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-16 09:41:45 +0545

    Add list manipulation functionality to Basics\list.py

---
+++ Basics/list.py
@@ -0,0 +1,13 @@
+names = ['Alice', 'Bob', 'Charlie']
+
+print(names[0])  # Alice
+print(names[1])  # Bob
+print(names[2])  # Charlie
+
+names[0] = 'Ashim'
+
+print(names[0])  # Ashim
+print(names)
+
+print(len(names))  # 3
+
