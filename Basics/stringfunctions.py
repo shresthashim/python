01552849ce3ea9ca026fe9cde7b5e1dc7645c540
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-16 09:24:47 +0545

    Add string manipulation functions to Basics\stringfunctions.py

---
+++ Basics/stringfunctions.py
@@ -0,0 +1,11 @@
+sentence = "I am a sentence."
+
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.title())
+print(sentence.swapcase())
+print(sentence.replace("a", "another"))
+
+
+print("am" in sentence)
