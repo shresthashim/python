d8272699dc3b0a62c4a8eef2c60efc7f948685f8
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-16 09:23:38 +0545

    Add user input and calculate sum of two numbers

---
+++ Basics/conversion.py
@@ -0,0 +1,10 @@
+first = input("Enter first number: ")
+
+# The input() function always returns a string. To convert it to an integer, use the int() function.
+
+
+second = input("Enter second number: ")
+
+sum  = int(first) + int(second)
+
+print("The sum is " + str(sum))
